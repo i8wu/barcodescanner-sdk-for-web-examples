397722d5b3e06120f92756981e642d01844d969e
Lorenzo Wölckner <lorenzo@scandit.com>
2018-11-02 13:30:15 +0100

    Changed Angular sample

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,19 @@
+// tslint:disable:missing-jsdoc
+
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ScanditConfigProvider, ScanditPickerComponent, ScanditService } from "scandit-sdk-angular";
+import { AppComponent } from "./app.component";
+
+// tslint:disable-next-line:max-line-length
+const licenseKey: string = "YOUR_LICENSE_KEY_IS_NEEDED_HERE";
+const engineLocation: string = "https://unpkg.com/scandit-sdk/build";
+
+@NgModule({
+  declarations: [AppComponent, ScanditPickerComponent],
+  imports: [BrowserModule],
+  providers: [ScanditService, ScanditConfigProvider(licenseKey, engineLocation)],
+  bootstrap: [AppComponent]
+})
+// tslint:disable-next-line:no-unnecessary-class
+export class AppModule {}
--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,7 +1,8 @@
-import { enableProdMode } from '@angular/core';
+// tslint:disable:missing-jsdoc
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+
 platformBrowserDynamic().bootstrapModule(AppModule)
+  // tslint:disable-next-line:ter-arrow-body-style
   .catch(err => console.log(err));
